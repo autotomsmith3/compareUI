e9a3e7fd7a8e0cd0e40b825b24d6dc4bd6fa15a6
Zhoul <Zhoul@ZHOUL-LT.autodatacorp.org>
2018-01-05 10:49:30 -0500

    Updated for removing 4 colorCodes.

--- src/cig/ReadMappings.java
+++ src/cig/ReadMappings.java
@@ -1,6 +1,6 @@
 package cig;
 
-//Reading and writing Work well on Dec 28, 2017. Submitted on Dec 28, 2017.
+//Reading from a file and writing to a file functionalities Work well on Dec 28, 2017. Submitted on Dec 28, 2017.
 //Description: It can retrieve all rows by styleids, modify the code elephantList.get(17) to get all OEM="~Y~"
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -55,12 +55,16 @@ public class ReadMappings {
 					List<String> elephantList = Arrays.asList(line.split(","));
 					StyleID_0 = elephantList.get(0);
 					// StyleID_0=elephantList.get(17);
+					Ext1MfrFullCode_3=elephantList.get(3);
 
 					if (StyleID_0.equalsIgnoreCase(styleid)) {
 						// if (StyleID_0.equalsIgnoreCase("~Y~")) {
+						if (Ext1MfrFullCode_3.equalsIgnoreCase("~BX~")||Ext1MfrFullCode_3.equalsIgnoreCase("~GR~")||Ext1MfrFullCode_3.equalsIgnoreCase("~GX~")||Ext1MfrFullCode_3.equalsIgnoreCase("~BK~")) {
+							System.out.println("Skip color code:"+Ext1MfrFullCode_3);
+						}else {
 						bufferedWriter.write(line);
 						bufferedWriter.newLine();
-
+						}
 						System.out.println(line);
 						// System.out.println(elephantList);
 
@@ -154,29 +158,39 @@ public class ReadMappings {
 	}
 
 	public static void main(String[] args) {
-		
-//		//US mapping *****************************************************
-		String inputFileName_color = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\US\\unzipped\\US_ImageGalleryMapping_2017_Color.txt";
-		String inputFileName_vehicle = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\US\\unzipped\\US_ImageGalleryMapping_2017_Vehicle.txt";
 
-		String outputFileName_color = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\US\\unzipped\\StyleID_US_ImageGalleryMapping_2017_Color.txt";
-		String outputFileName_vehicle = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\US\\unzipped\\StyleID_US_ImageGalleryMapping_2017_Vehcile.txt";
+		// //US mapping *****************************************************
+//		String inputFileName_color = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\US\\unzipped\\US_ImageGalleryMapping_2017_Color.txt";
+//		String inputFileName_vehicle = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\US\\unzipped\\US_ImageGalleryMapping_2017_Vehicle.txt";
+		String inputFileName_color = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\US\\unzipped\\US_ImageGalleryMapping_2016_Color.txt";
+		String inputFileName_vehicle = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\US\\unzipped\\US_ImageGalleryMapping_2016_Vehicle.txt";
+		
+//		String outputFileName_color = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\US\\unzipped\\TestData_For_TC213119\\StyleIDs_44_US_2017_Ford_F150\\US_ImageGalleryMapping_2017_Color.txt";
+//		String outputFileName_vehicle = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\US\\unzipped\\TestData_For_TC213119\\StyleIDs_44_US_2017_Ford_F150\\US_ImageGalleryMapping_2017_Vehicle.txt";
+		String outputFileName_color = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\US\\unzipped\\TestData_For_TC213119\\StyleIDs_8_US_2016_Acura_MDX\\Removed_4_Colors_BasedOn_AllColors\\US_ImageGalleryMapping_2016_Color.txt";
+		String outputFileName_vehicle = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\US\\unzipped\\TestData_For_TC213119\\StyleIDs_8_US_2016_Acura_MDX\\Removed_4_Colors_BasedOn_AllColors\\US_ImageGalleryMapping_2016_Vehicle.txt";
+//		String[] styleIDs = { "387896" };// "387896","374391","389544" truck, suv, chrysler
+		//All 44 styleids for 2017 US Ford F150 Trucker
+//		String[] styleIDs = { "387896","387897","387898","387899","387900","387901","387902","387903","387904","387905","387906","387907","387908","387909","387910","387911","387912","387913","387914","387915","387916","387917","387918","387919","387920","387921","387922","387923","387924","387925","387926","387927","387928","387929","387930","387931","387932","387933","387934","387935","387936","387937","388260","388261"};
+		//All 8 styleids for 2016 Acura SUV MDX
+		String[] styleIDs ={"374380","374391","374381","374392","374382","374388","374389","374390"};
+		
+		
+		
+		// //US mapping *****************************************************
 
-		String[] styleIDs = { "387896", "381489" };// "387896","374391","389544"
-//		//US mapping *****************************************************
-		
-//		//CA mapping *****************************************************
-//		String inputFileName_color = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\CA\\unzipped\\CA_ImageGalleryMapping_2017_Color.txt";
-//		String inputFileName_vehicle = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\CA\\unzipped\\CA_ImageGalleryMapping_2017_Vehicle.txt";
-//
-//		String outputFileName_color = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\CA\\unzipped\\StyleID_CA_ImageGalleryMapping_2017_Color.txt";
-//		String outputFileName_vehicle = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\CA\\unzipped\\StyleID_CA_ImageGalleryMapping_2017_Vehcile.txt";
-//
-//		String[] styleIDs = { "389544" };// "387896","374391","389544"
-//		//CA mapping *****************************************************		
-		
-//		ReadColor(inputFileName_color, outputFileName_color, styleIDs);
-		 ReadVehicle(inputFileName_vehicle, outputFileName_vehicle, styleIDs);
+		// //CA mapping *****************************************************
+		// String inputFileName_color = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\CA\\unzipped\\CA_ImageGalleryMapping_2017_Color.txt";
+		// String inputFileName_vehicle = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\CA\\unzipped\\CA_ImageGalleryMapping_2017_Vehicle.txt";
+		//
+		// String outputFileName_color = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\CA\\unzipped\\StyleID_CA_ImageGalleryMapping_2017_Color.txt";
+		// String outputFileName_vehicle = "C:\\chrome\\loader\\CIG-MAPPING-IMPORT\\zipfiles\\CA\\unzipped\\StyleID_CA_ImageGalleryMapping_2017_Vehcile.txt";
+		//
+		// String[] styleIDs = { "389544" };// "387896","374391","389544"
+		// //CA mapping *****************************************************
+
+		ReadColor(inputFileName_color, outputFileName_color, styleIDs);
+		ReadVehicle(inputFileName_vehicle, outputFileName_vehicle, styleIDs);
 		System.out.println("Done!");
 	}
 
