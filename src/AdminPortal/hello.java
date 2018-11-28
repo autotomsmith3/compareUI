bade2e14d24a5cad760bf2ff03ebbf12b667fe2c
Zhoul <Zhoul@LZHOULT.autodatacorp.org>
2018-11-28 09:28:18 -0500

    Added hello for testing

---
+++ src/AdminPortal/hello.java
@@ -0,0 +1,15 @@
+package AdminPortal;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i < 150; i++) {
+			if (i % 12 == 0) {
+				System.out.println("I="+i);
+			}
+		}
+		System.out.println("complete!");
+	}
+
+}
