401401e4e4f8785628fa5c41423f732d0e57ffbf
lucas.zhou <lucas.zhou@autodata.net>
2020-10-17 15:33:31 -0400

    Add sending multiple emails

--- src/Mail/SendEmail.java
+++ src/Mail/SendEmail.java
@@ -13,10 +13,10 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class SendEmail {
-	public static void sendEmails(String gmailUsername, String gmailPassword, String[] emails, String Subject,
+	public static void sendEmails(String gmailUsername, String gmailPassword, String[] toEmails, String Subject,
 			String content) {
-		final String username = "sautotomsmithjj@gmail.com";
-		final String password = "Autodata1";
+//		final String username = "sautotomsmithjj@gmail.com";
+//		final String password = "Autodata1";
 
 		Properties props = new Properties();
 		props.put("mail.smtp.starttls.enable", "true");
@@ -26,35 +26,37 @@ public class SendEmail {
 
 		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
+				return new PasswordAuthentication(gmailUsername, gmailPassword);
 			}
 		});
-		int emailNumber = emails.length;
+		int emailNumber = toEmails.length;
 		// Integer.toString(emailNumber)
 
 		try {
-			for (emailNumber = 0; emailNumber <= 1; emailNumber++) {
+			for (int i = 0; i <= emailNumber - 1; i++) {
 				Message message = new MimeMessage(session);
-				message.setFrom(new InternetAddress("sautotomsmithjj@gmail.com"));
-				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emails[emailNumber]));
-				message.setSubject("Application Failure Alert Testing...ignore please.");
+				message.setFrom(new InternetAddress(gmailUsername));
+				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmails[i]));
+				message.setSubject(Subject);
 				message.setText(content);
 				Transport.send(message);
 			}
-			System.out.println("Done");
+			System.out.println("\n Sent " + (emailNumber) + " Email(s). Done!");
 		} catch (MessagingException e) {
+			System.out.println("\n Failed to send " + (emailNumber) + " Email(s). Done!");
 			throw new RuntimeException(e);
 		}
 	}
+
 	public static void main(String[] args) {
 		String gmailUsername = "sautotomsmithjj@gmail.com";
 		String gmailPassword = "Autodata1";
-		String[] emails = { "lucas.zhou@autodatasolutions.com", "sautotomsmithjj@gmail.com" };
+		String[] sendEmailsList = { "lucas.zhou@autodatasolutions.com" };// "lucas.zhou@autodatasolutions.com", "sautotomsmithjj@gmail.com"
 //		int emailNumber = emails.length;
 		String Subject = "Application Failure Alert Testing...ignore please.";
 		String content = "Hello,\n\n Please ignore this email. This is testing alert from monitor application!";
 
-		sendEmails(gmailUsername, gmailPassword, emails, Subject, content);
+		sendEmails(gmailUsername, gmailPassword, sendEmailsList, Subject, content);
 		System.out.println("\n\n*****************Complete!*******************\n");
 	}
 
