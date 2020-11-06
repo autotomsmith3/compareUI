ef3432bc8198562f8033872c27d02f31aa56757b
lucas.zhou <lucas.zhou@autodata.net>
2020-11-06 11:09:18 -0500

    No change 2

--- src/competitiveCompareUI/competitiveCompareUIController.java
+++ src/competitiveCompareUI/competitiveCompareUIController.java
@@ -171,7 +171,7 @@ public class competitiveCompareUIController extends Comlibs {
 	}
 
 	public static void main(String[] args) throws Exception {
-		// Load environment parameters
+		// Load environment parameters 
 		Properties prop = new Properties();
 		try {
 			prop.load(competitiveCompareUIController.class.getClassLoader()
