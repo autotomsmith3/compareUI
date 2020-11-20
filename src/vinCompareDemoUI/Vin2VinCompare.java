2f9404c5957f3a56de029488117092cdccad85bd
lucas.zhou <lucas.zhou@autodata.net>
2020-11-19 20:19:06 -0500

    First initial vinCompareDemo

---
+++ src/vinCompareDemoUI/Vin2VinCompare.java
@@ -0,0 +1,242 @@
+package vinCompareDemoUI;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Vin2VinCompare extends Comlibs {
+	private final WebDriver driver;
+
+	public Vin2VinCompare(WebDriver driver) throws IOException {
+		this.driver = driver;
+		// String wh1=driver.getWindowHandle();
+		String sPageTitle = "VIN 2 VIN Compare";
+		boolean existTitle = TitleDisplay(driver, sPageTitle);
+		if (existTitle) {
+			rwExcel("", true, "Page Title is displayed", sPageTitle);
+		} else {
+			rwExcel("", false, "Page title is NOT showing properly. Exceed time limit!",
+					"The page title is NOT - " + sPageTitle);
+		}
+		if (!sPageTitle.equals(driver.getTitle())) {
+			System.out.println("Page title does not matche. Expected page title =" + sPageTitle);
+		}
+	}
+
+	By vinBtn = By.xpath("/html/body/div[1]/form/div/div[1]/div[1]/h4/a");
+	By vehicleInfoBtn = By.xpath("//*[@id=\"App\"]/div/div[2]/div/div/article/div/div[2]/div[1]/div/div/span/label[2]");
+	By vinDropListBtn = By
+			.xpath("//*[@id=\"App\"]/div/div[2]/div/div/article/div/div[2]/div[2]/div/div/form/div[1]/div[2]/div/div");
+
+	By ConfigurableParameters = By.xpath("/html/body/div[1]/form/div/div[1]/div[1]/h4/a");
+	By CompareScriptURL = By.xpath("//*[@id=\"compare-script-url\"]");
+	By CompareAPIBaseURL = By.xpath("//*[@id=\"data-compare-url\"]");
+	By AccessToken = By.xpath("//*[@id=\"data-access-token\"]");
+	By Profile = By.xpath("//*[@id=\"data-profile\"]");
+	By ProductKey = By.xpath("//*[@id=\"data-product-key\"]");
+	By Locale = By.xpath("//*[@id=\"data-locale\"]");
+	By CompareBtn = By.xpath("/html/body/div[1]/form/button[1]");
+	By EditConfiguration = By.xpath("/html/body/div[1]/form/button[2]");
+	By NewConfiguration = By.xpath("/html/body/div[1]/form/button[3]");
+//	By xxxxxx = By.xpath("");	
+//	By xxxxxx = By.xpath("");	
+//	By xxxxxx = By.xpath("");	
+//	By xxxxxx = By.xpath("");
+
+	public Vin2VinCompare clickConfigurealeParameters(WebDriver driver, String tc) throws Exception {
+		elementExist(driver, ConfigurableParameters, true, tc);
+		driver.findElement(ConfigurableParameters).click();
+		return this;
+	}
+
+	public Vin2VinCompare inputCompareScriptURL(WebDriver driver, String un, String tc) throws Exception {
+		elementExist(driver, CompareScriptURL, true, tc);
+		driver.findElement(CompareScriptURL).clear();
+		driver.findElement(CompareScriptURL).sendKeys(un);
+		return this;
+	}
+
+	public Vin2VinCompare inputCompareAPIBaseURL(WebDriver driver, String un, String tc) throws Exception {
+		elementExist(driver, CompareAPIBaseURL, true, tc);
+		driver.findElement(CompareAPIBaseURL).clear();
+		driver.findElement(CompareAPIBaseURL).sendKeys(un);
+		return this;
+	}
+
+	public Vin2VinCompare inputAccessToken(WebDriver driver, String un, String tc) throws Exception {
+		elementExist(driver, AccessToken, true, tc);
+		driver.findElement(AccessToken).clear();
+		driver.findElement(AccessToken).sendKeys(un);
+		return this;
+	}
+
+	public Vin2VinCompare inputProfile(WebDriver driver, String un, String tc) throws Exception {
+		elementExist(driver, Profile, true, tc);
+		driver.findElement(Profile).clear();
+		driver.findElement(Profile).sendKeys(un);
+		return this;
+	}
+
+	public Vin2VinCompare inputProductKey(WebDriver driver, String un, String tc) throws Exception {
+		elementExist(driver, ProductKey, true, tc);
+		driver.findElement(ProductKey).clear();
+		driver.findElement(ProductKey).sendKeys(un);
+		return this;
+	}
+
+	public Vin2VinCompare inputLocale(WebDriver driver, String un, String tc) throws Exception {
+		elementExist(driver, Locale, true, tc);
+		driver.findElement(Locale).clear();
+		driver.findElement(Locale).sendKeys(un);
+		return this;
+	}
+
+//	public Vin2VinCompare inputVins(WebDriver driver, String [] vins, String [] DealerPrices, String[] images, String tc)throws Exception  {
+//		
+//		
+//		
+//		elementExist(driver, Locale, true, tc);
+//		driver.findElement(Locale).clear();
+//		driver.findElement(Locale).sendKeys(un);
+//		return this;
+//	}
+	public Vin2VinCompare inputVins(WebDriver driver, String vins[], String tc) throws Exception {
+		int n = vins.length;
+
+		for (int i = 1; i <= n; i++) {
+
+			By VIN = By.xpath("//*[@id='vin-" + i + "']");
+
+			elementExist(driver, VIN, true, tc);
+			driver.findElement(VIN).clear();
+			driver.findElement(VIN).sendKeys(vins[i - 1]);
+
+		}
+		return this;
+	}
+
+	public Vin2VinCompare inputDealerPrices(WebDriver driver, String vins[], String DealerPrices[], String Images[],
+			String tc) throws Exception {
+		int n = vins.length;
+
+		for (int i = 1; i <= n; i++) {
+
+			By PlusBtn = By.xpath("//*[@id='custom-vin-button-" + i + "']");
+			By DealerPrice = By.xpath("//*[@id='vin-dealer-price-" + i + "']");
+			By Image = By.xpath("//*[@id='vin-image-" + i + "']");
+//			click +
+			elementExist(driver, PlusBtn, true, tc);
+			driver.findElement(PlusBtn).click();
+
+//			enter dealerprices:
+			elementExist(driver, DealerPrice, true, tc);
+			driver.findElement(DealerPrice).clear();
+			driver.findElement(DealerPrice).sendKeys(DealerPrices[i - 1]);
+
+//			enter image urls:
+			elementExist(driver, Image, true, tc);
+			driver.findElement(Image).clear();
+			driver.findElement(Image).sendKeys(Images[i - 1]);
+
+		}
+		return this;
+	}
+
+	public Vin2VinCompare clickCompareBtn(WebDriver driver, String tc) throws Exception {
+		elementExist(driver, CompareBtn, true, tc);
+		driver.findElement(CompareBtn).click();
+		return this;
+	}
+
+	public Vin2VinCompare clickEditConfigurationBtn(WebDriver driver, String tc) throws Exception {
+		elementExist(driver, EditConfiguration, true, tc);
+		driver.findElement(EditConfiguration).click();
+		return this;
+
+	}
+
+	public Vin2VinCompare clickNewConfigurationBtn(WebDriver driver, String tc) throws Exception {
+		elementExist(driver, NewConfiguration, true, tc);
+		driver.findElement(NewConfiguration).click();
+		return this;
+	}
+
+	public void verifyDealerPriceWhichIsNot0(WebDriver driver, String dealerPrice, String tc) throws Exception {
+		By dealerPrice1 = By.xpath("/html/body/div[2]/div/div[2]/div[2]/div[2]/div/div[4]/div[2]/div/span");
+		By dealerPrice2 = By.xpath("/html/body/div[2]/div/div[2]/div[2]/div[2]/div/div[4]/div[3]/span");
+		By dealerPrice3 = By.xpath("/html/body/div[2]/div/div[2]/div[2]/div[2]/div/div[4]/div[4]/div/span");
+		
+		int dp1=Integer.parseInt(dealerPrice);
+		if (dp1==0) {
+			rwExcel(tc, false, "Verify Deapler Price for VIN1", "Dealer Price is 0 which cannot be verify in this test case. Change the deapler price in properties file to make this test valid!");
+			return;
+		}
+		elementExist(driver, dealerPrice1, true, tc);
+		String dp1String =driver.findElement(dealerPrice1).getText();
+		dp1String=dp1String.replace("$","");
+		dp1String=dp1String.replace(",","");
+		int dp1Int=Integer.parseInt(dp1String);
+		
+		if (dp1==dp1Int) {
+			rwExcel(tc, true, "Verify Deapler Price for VIN1", "Dealer Price 1="+dp1String);
+			
+		}else {
+			rwExcel(tc, false, "Verify Deapler Price for VIN1", "Dealer Price from page shows:"+dp1Int+". Expected Dealer Price 1="+dealerPrice1);
+		}
+	}
+	public void verifyDealerPriceWhichIs0(WebDriver driver, String dealerPrice, String tc) throws Exception {
+		By dealerPrice2 = By.xpath("/html/body/div[2]/div/div[2]/div[2]/div[2]/div/div[4]/div[3]/span");
+		By dealerPrice3 = By.xpath("/html/body/div[2]/div/div[2]/div[2]/div[2]/div/div[4]/div[4]/div/span");
+		String dp2="Contact Dealer";
+		int dp1=Integer.parseInt(dealerPrice);//0
+		if (!(dp1==0)) {
+			rwExcel(tc, false, "Verify Deapler Price for VIN1 which is 0", "Dealer Price is not 0 which cannot be verify in this test case. Change the deapler price in properties file to make this test valid!");
+			return;
+		}else {
+			
+		}
+		elementExist(driver, dealerPrice2, true, tc);
+		String dp1String =driver.findElement(dealerPrice2).getText();
+		if (dp1String.equalsIgnoreCase(dp2)) {
+			rwExcel(tc, true, "Verify Deapler Price for VIN2", "Dealer Price 2 = "+dealerPrice+". Page shows: "+dp2);
+		}else {
+			rwExcel(tc, false, "Verify Deapler Price for VIN2 which is 0", "Dealer Price from page shows:"+"\""+dp1String+"\""+". Expected Dealer Price 2 = "+"\""+dp2+"\"");
+		}
+		
+//		int dp1frPage=
+//		dp1String =driver.findElement(dealerPrice1).getCssValue("span");
+//		dp1String =driver.findElement(dealerPrice1).getTagName();
+		
+	}
+
+	public void verifyVin1Image(WebDriver driver, String tc) throws Exception {
+		
+		By Vin1imageLocator=By.xpath("/html/body/div[2]/div/div[1]/div[2]/div[2]/div[2]/div[1]/span/img");
+		VerifyImageLoaded(driver, Vin1imageLocator, tc);
+	}
+	public void verifyVin2Image(WebDriver driver, String tc) throws Exception {
+		
+		By Vin1imageLocator=By.xpath("/html/body/div[2]/div/div[1]/div[2]/div[2]/div[2]/div[2]/span/img");
+		VerifyImageLoaded(driver, Vin1imageLocator, tc);
+	}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/vinCompareDemoUI/vinCompareUIController.java
@@ -0,0 +1,281 @@
+package vinCompareDemoUI;
+
+//Moved from perforce:1666..
+/**
+* @author Zhoul
+* Initial date:  -2020-10-09
+* Modified by ...
+* In case of broken, update may needed:
+*  1. Selenium Webdriver
+*  2. Chromedriver
+*  3. geckodriver (FF)
+*  4. poi-3.9-20121203.jar
+*  5. sqljdbc4.jar
+*  6. javamail.jar
+*  7.json-20160212.jar
+*  8. bson-3.30.jar
+*  
+*   
+*   */
+
+import java.awt.AWTException;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+//Test updated 02
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class vinCompareUIController extends Comlibs {
+	private final WebDriver driver;
+	final int wt_Secs = 0;
+	static String[] vinpxConent = new String[200];
+	static String[] stockpxConent = new String[200];
+	static String[] lotpxConent = new String[200];
+	final static int SINGLE_VIN_RENDER_MAX_WT = 10;
+	final static int ALL_VINS_RENDER_MAX_WT = 20;
+
+	public vinCompareUIController(WebDriver driver, String myId) throws IOException {
+		this.driver = driver;
+		// String wh1=driver.getWindowHandle();
+
+		String sPageTitle = "VINpx Login";
+		boolean existTitle = TitleDisplay(driver, sPageTitle);
+		if (existTitle)
+			try {
+				{
+					rwExcel("", true, "Page Title is displayed!...", sPageTitle);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		else {
+			rwExcel("", false, "Page title is NOT showing properly. Exceed time limit!",
+					"The page title is NOT - " + sPageTitle);
+		}
+		if (!sPageTitle.equals(driver.getTitle())) {
+			// throw new IllegalStateException("The page title is NOT - "
+			// + sPageTitle);
+			System.out.println("The page title is NOT - " + sPageTitle);
+		}
+	}
+
+	/*
+	 * ------------------------------ Home Page Object Repositories: ------------------------------
+	 */
+	By dealershipNameField = By.id("dealerName");
+	By webSite = By.id("dealerSite");
+
+	static String urlLink;
+
+	public static Vin2VinCompare loadURL(WebDriver driver, String bURL) throws IOException {
+		driver.get(bURL);
+		return new Vin2VinCompare(driver);
+	}
+
+	private static String[] fetchOneDemArrayFromPropFile(String propertyName, Properties propFile) {
+
+		// get array split up by the colin
+		String a[] = propFile.getProperty(propertyName).split(",");
+
+		return a;
+	}
+
+	public static void Vin2VinCompareTcs(WebDriver driver, String brw, String envment) throws Exception {
+		// Load environment parameters
+		Properties prop = new Properties();
+		// testprop.load(new FileInputStream("data/autopxConf.properties"));
+		try {
+			prop.load(vinCompareUIController.class.getClassLoader().getResourceAsStream("./data/vinCompare.properties"));// "./main.properties";
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		String env = envment;
+		String tBrowser = brw;
+		String envDevice = prop.getProperty("Compare.envDevice");
+		String onScreen = prop.getProperty("Compare.onScreen");
+		String VinCompareUIURL = prop.getProperty(env + ".VinCompareUIURL");
+
+		String CompareScriptURL = prop.getProperty(env + ".CompareScriptURL");
+
+		String CompareAPIBaseURL = prop.getProperty(env + ".CompareAPIBaseURL");
+
+		String AccessToken = prop.getProperty(env + ".AccessToken");
+
+		String Profile = prop.getProperty(env + ".Profile");
+		String ProductKey = prop.getProperty(env + ".ProductKey");
+		String Locale = prop.getProperty(env + ".Locale");
+
+		String vins[] = fetchOneDemArrayFromPropFile(env + ".CompareVINs", prop);
+		int numOfVins = vins.length;
+
+		String DealerPrices[] = fetchOneDemArrayFromPropFile(env + ".DealerPrices", prop);
+		int numOfDealerPrices = vins.length;
+
+		String Image1 = prop.getProperty(env + ".Image1");
+		String Image2 = prop.getProperty(env + ".Image2");
+		String Image3 = prop.getProperty(env + ".Image3");
+		String Image4 = prop.getProperty(env + ".Image4");
+		String Image5 = prop.getProperty(env + ".Image5");
+		String Image6 = prop.getProperty(env + ".Image6");
+
+		String[] Images = { Image1, Image2, Image3, Image4, Image5, Image6 };
+
+		int wt = Integer.parseInt(prop.getProperty("Compare.waitTime"));
+		// Initial
+		String tc;
+		String ptitle;
+		Comlibs log = new Comlibs();
+		log.rwExcel("", "*********VIN Compare Tester Tool UI**********", "");
+		Vin2VinCompare Vin2VinComparePage = new Vin2VinCompare(driver);
+		tc = "TCxxxx_01";
+		Vin2VinComparePage.clickConfigurealeParameters(driver, tc);
+
+		tc = "TCxxxx_021";
+		Vin2VinComparePage.inputCompareScriptURL(driver, CompareScriptURL, tc);
+		tc = "TCxxxx_022";
+		Vin2VinComparePage.inputCompareAPIBaseURL(driver, CompareAPIBaseURL, tc);
+
+		tc = "TCxxxx_023";
+		Vin2VinComparePage.inputAccessToken(driver, AccessToken, tc);
+
+		tc = "TCxxxx_024";
+		Vin2VinComparePage.inputProfile(driver, Profile, tc);
+		tc = "TCxxxx_025";
+		Vin2VinComparePage.inputProductKey(driver, ProductKey, tc);
+
+		tc = "TCxxxx_026";
+		Vin2VinComparePage.inputLocale(driver, Locale, tc);
+
+		tc = "Enter VINS_027";
+
+		Vin2VinComparePage.inputVins(driver, vins, tc);
+
+		tc = "Enter DealerPrices_028";
+		Vin2VinComparePage.inputDealerPrices(driver, vins, DealerPrices, Images, tc);//
+//		
+		tc = "Click on Compare button_029";
+		Vin2VinComparePage.clickCompareBtn(driver, tc);
+		log.Wait(wt*2);
+//	*********************************************************
+		tc = "Click on Edit Configuration button_030";
+		Vin2VinComparePage.clickEditConfigurationBtn(driver, tc);
+		log.Wait(wt*2);
+		tc = "Click on Compare button_0291";
+		Vin2VinComparePage.clickCompareBtn(driver, tc);
+		log.Wait(wt*2);
+
+		tc = "Click on New Configuration button_031";
+		Vin2VinComparePage.clickNewConfigurationBtn(driver, tc);
+		log.Wait(wt*2);
+//		Check others
+		tc = "Click on Compare button_032";
+		Vin2VinComparePage.clickCompareBtn(driver, tc);
+		log.Wait(wt*2);
+//		*********************************************************
+		tc = "Verify Dealer Price Vin1 01";
+		Vin2VinComparePage.verifyDealerPriceWhichIsNot0(driver, DealerPrices[0], tc);
+		tc = "Verify Dealer Price Vin2 02";
+		Vin2VinComparePage.verifyDealerPriceWhichIs0(driver, DealerPrices[1], tc);
+		tc = "Verify VIN 1 image 01";
+		Vin2VinComparePage.verifyVin1Image(driver, tc);
+		tc = "Verify VIN 2 image 02";
+		Vin2VinComparePage.verifyVin2Image(driver, tc);
+	}
+
+	public static void main(String[] args) throws Exception {
+		// Load environment parameters
+		Properties prop = new Properties();
+		try {
+			prop.load(vinCompareUIController.class.getClassLoader().getResourceAsStream("./data/vinCompare.properties"));// "./main.properties";
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String env = prop.getProperty("Compare.environment");
+		String tBrowser = prop.getProperty("Compare.browser");
+		String envDevice = prop.getProperty("Compare.envDevice");
+		String onScreen = prop.getProperty("Compare.onScreen");
+		String VinCompareUIURL = prop.getProperty(env + ".VinCompareUIURL");
+		String vins[] = fetchOneDemArrayFromPropFile(env + ".CompareVINs", prop);
+		String [] Devices=new String[3];
+		
+		if (envDevice.equalsIgnoreCase("PC")) {
+			Devices[0]= "PC";
+			Devices[1]= "Tablet";
+			Devices[2]= "Smartphone";
+		}else if (envDevice.equalsIgnoreCase("Tablet")) {
+			Devices[0]= "Tablet";
+			Devices[1]= "Smartphone";
+			Devices[2]= "PC";
+		}else if (envDevice.equalsIgnoreCase("Smartphone")) {
+			Devices[0]= "Smartphone";
+			Devices[1]= "PC";
+			Devices[2]= "Tablet";
+		}else {
+			Devices[0]= "PC";
+			Devices[1]= "Tablet";
+			Devices[2]= "Smartphone";
+		}
+		
+		// i=3: all 3 devices
+		for (int i = 0; i < 1; i++) {
+			System.out.println("Testing is started in " + env + "\n");
+			// Initial
+			Comlibs log = new Comlibs();
+			final WebDriver driver;
+			driver = log.drivers(tBrowser);// Firefox, Chrome
+			driver.manage().deleteAllCookies();
+			System.out.println("Test Browser = " + tBrowser + "\n");
+			System.out.println("Test Device = " + Devices[i] + "\n");
+
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			if (!tBrowser.equalsIgnoreCase("Chromexxxxxxxxx")) {
+				log.SelecBroswerResolution(driver,  Devices[i], onScreen);
+			}
+			log.rwExcel("", "****** Testing started ******" + (i + 1), "");
+			log.rwExcel("", "Test Browser", tBrowser);
+			log.rwExcel("", "Test Environment", env);
+			log.rwExcel("", "Test Devicer",  Devices[i] );
+			
+			loadURL(driver, VinCompareUIURL);
+			String vin1 = vins[0];
+			//// 1. VIN Compare Tester Tool UI page
+			log.rwExcel("", "-----VIN Compare Tester Tool UI page Testing started-----" + (i + 1), "");
+//			1. ***********VIN Compare**************
+			Vin2VinCompareTcs(driver, tBrowser, env);
+//			 ***********VIN Compare**************
+
+			log.rwExcel("", "****** Testing is complete ****** " + (i + 1), "");
+			driver.close();
+			System.out.println("Test is complete!!!   i = " + (i + 1)+"\n");
+		}
+		System.out.println("*****************All Tests are done!!!*****************"+"\n");
+		return;
+	}
+}
