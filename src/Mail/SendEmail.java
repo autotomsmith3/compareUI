ccfa613c9309ba52b82a0001f0595ac13d132309
lucas.zhou <lucas.zhou@autodata.net>
2020-12-15 13:42:43 -0500

    Add sendEmail var

--- src/Mail/SendEmail.java
+++ src/Mail/SendEmail.java
@@ -16,36 +16,38 @@ import javax.mail.internet.MimeMessage;
 import competitiveCompareUI.competitiveCompareUIController;
 
 public class SendEmail {
-	public static void sendEmails(String gmailUsername, String gmailPassword, String[] toEmails, String Subject,
-			String content) {
+	public static void sendEmails(String sendEmail, String gmailUsername, String gmailPassword, String[] toEmails,
+			String Subject, String content) {
+		if (sendEmail.equalsIgnoreCase("Yes")) {
 //		final String username = "sautotomsmithjj@gmail.com";
 //		final String password = "Autodata1";
 
-		Properties props = new Properties();
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
+			Properties props = new Properties();
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "587");
 
-		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(gmailUsername, gmailPassword);
+			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(gmailUsername, gmailPassword);
+				}
+			});
+			int emailNumber = toEmails.length;
+			try {
+				for (int i = 0; i <= emailNumber - 1; i++) {
+					Message message = new MimeMessage(session);
+					message.setFrom(new InternetAddress(gmailUsername));
+					message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmails[i]));
+					message.setSubject(Subject);
+					message.setText(content);
+					Transport.send(message);
+				}
+				System.out.println("\n Sent " + (emailNumber) + " Email(s). Done!");
+			} catch (MessagingException e) {
+				System.out.println("\n Failed to send " + (emailNumber) + " Email(s). Done!");
+				throw new RuntimeException(e);
 			}
-		});
-		int emailNumber = toEmails.length;
-		try {
-			for (int i = 0; i <= emailNumber - 1; i++) {
-				Message message = new MimeMessage(session);
-				message.setFrom(new InternetAddress(gmailUsername));
-				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmails[i]));
-				message.setSubject(Subject);
-				message.setText(content);
-				Transport.send(message);
-			}
-			System.out.println("\n Sent " + (emailNumber) + " Email(s). Done!");
-		} catch (MessagingException e) {
-			System.out.println("\n Failed to send " + (emailNumber) + " Email(s). Done!");
-			throw new RuntimeException(e);
 		}
 	}
 
@@ -57,12 +59,14 @@ public class SendEmail {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		String sendEmail = prop.getProperty("CompetitiveCompare.sendEmail");
 		String gmailUsername = prop.getProperty(env + ".gmailUsername");
 		String gmailPassword = prop.getProperty(env + ".gmailPassword");
 		String sendEmailsList[] = fetchOneDemArrayFromPropFile(env + ".sendEmailsList", prop);
 		String Subject = prop.getProperty(env + ".Subject");
 		String content = prop.getProperty(env + ".content");
-		sendEmails(gmailUsername, gmailPassword, sendEmailsList, env+" - "+brand + " - " + Subject, content+"\n\n\n URL: "+urlString);
+		sendEmails(sendEmail, gmailUsername, gmailPassword, sendEmailsList, env + " - " + brand + " - " + Subject,
+				content + "\n\n\n URL: " + urlString);
 		System.out.println("\n\n*****************Sending Email is Complete!*******************\n");
 	}
 
@@ -73,13 +77,15 @@ public class SendEmail {
 	}
 
 	public static void main(String[] args) {
+		String sendEmail = "Yes";
 		String gmailUsername = "sautotomsmithjj@gmail.com";
 		String gmailPassword = "Autodata1";
 		String[] sendEmailsList = { "lucas.zhou@autodatasolutions.com" };
 		String Subject = "Application Failure Alert Testing...ignore please.";
 		String content = "Hello,\n\n Please ignore this email. This is testing alert from monitor application!";
-		String urlString="http://qa1-compare.gm-test.autodata.tech/kia/ca/vehicle/#/select/primary/compare";
-		sendEmails(gmailUsername, gmailPassword, sendEmailsList, Subject, content+"\n\n\n URL: "+urlString);
+		String urlString = "http://qa1-compare.gm-test.autodata.tech/kia/ca/vehicle/#/select/primary/compare";
+		sendEmails(sendEmail, gmailUsername, gmailPassword, sendEmailsList, Subject,
+				content + "\n\n\n URL: " + urlString);
 		System.out.println("\n\n*****************Complete!*******************\n");
 	}
 
