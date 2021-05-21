78749c439103e96661029ee24098cae952eeb6ec
Zhoul <Zhoul@LZHOULT2.autodatacorp.org>
2021-05-21 17:41:27 -0400

    Fixed sending gmail with Gmail SSL. Working fine in the new Dell laptop.

--- src/Mail/SendEmail.java
+++ src/Mail/SendEmail.java
@@ -1,7 +1,6 @@
 package Mail;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -9,45 +8,41 @@ import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import competitiveCompareUI.competitiveCompareUIController;
 
 public class SendEmail {
-	public static void sendEmails(String sendToEmail, String gmailUsername, String gmailPassword, String[] toEmails,
+	public static void sendEmails(String gmailUsername, String gmailPassword, String toEmails, int emailNumber,
 			String Subject, String content) {
-		if (sendToEmail.equalsIgnoreCase("Yes")) {
-//		final String username = "sautotomsmithjj@gmail.com";
-//		final String password = "Autodata1";
 
-			Properties props = new Properties();
-			props.put("mail.smtp.starttls.enable", "true");
-			props.put("mail.smtp.auth", "true");
-			props.put("mail.smtp.host", "smtp.gmail.com");
-			props.put("mail.smtp.port", "587");
-
-			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(gmailUsername, gmailPassword);
-				}
-			});
-			int emailNumber = toEmails.length;
-			try {
-				for (int i = 0; i <= emailNumber - 1; i++) {
-					Message message = new MimeMessage(session);
-					message.setFrom(new InternetAddress(gmailUsername));
-					message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmails[i]));
-					message.setSubject(Subject);
-					message.setText(content);
-					Transport.send(message);
-				}
-				System.out.println("\n Sent " + (emailNumber) + " Email(s). Done!");
-			} catch (MessagingException e) {
-				System.out.println("\n Failed to send " + (emailNumber) + " Email(s). Done!");
-				throw new RuntimeException(e);
+		Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "465");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.socketFactory.port", "465");
+		prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(gmailUsername, gmailPassword);
 			}
+		});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(gmailUsername));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmails));
+			message.setSubject(Subject);
+			message.setText(content);
+			Transport.send(message);
+			System.out.println("\n Sent " + (emailNumber) + " Email(s). Done!");
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+			System.out.println("\n Failed to send " + (emailNumber) + " Email(s). Done!");
+			throw new RuntimeException(e);
 		}
 	}
 
@@ -55,38 +50,26 @@ public class SendEmail {
 		Properties prop = new Properties();
 		try {
 			prop.load(competitiveCompareUIController.class.getClassLoader()
-					.getResourceAsStream("./data/competitiveCompareGoAllTrims.properties"));
+					.getResourceAsStream("./data/competitiveCompare.properties"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		String sendToEmail = prop.getProperty("CompetitiveCompare.sendToEmail");
 		String gmailUsername = prop.getProperty(env + ".gmailUsername");
 		String gmailPassword = prop.getProperty(env + ".gmailPassword");
 		String sendEmailsList[] = fetchOneDemArrayFromPropFile(env + ".sendEmailsList", prop);
+		int emailNumber = sendEmailsList.length;
+		String sendEmailsString = prop.getProperty(env + ".sendEmailsList");
+
 		String Subject = prop.getProperty(env + ".Subject");
 		String content = prop.getProperty(env + ".content");
-		sendEmails(sendToEmail, gmailUsername, gmailPassword, sendEmailsList, env + " - " + brand + " - " + Subject,
+		sendEmails(gmailUsername, gmailPassword, sendEmailsString, emailNumber, env + " - " + brand + " - " + Subject,
 				content + "\n\n\n URL: " + urlString);
 		System.out.println("\n\n*****************Sending Email is Complete!*******************\n");
 	}
 
 	private static String[] fetchOneDemArrayFromPropFile(String propertyName, Properties propFile) {
-		// get array split up by the colin
 		String a[] = propFile.getProperty(propertyName).split(",");
 		return a;
-	}
-
-	public static void main(String[] args) {
-		String sendToEmail = "Yes";
-		String gmailUsername = "sautotomsmithjj@gmail.com";
-		String gmailPassword = "Autodata1";
-		String[] sendEmailsList = { "lucas.zhou@autodatasolutions.com" };
-		String Subject = "Application Failure Alert Testing...ignore please.";
-		String content = "Hello,\n\n Please ignore this email. This is testing alert from monitor application!";
-		String urlString = "http://qa1-compare.gm-test.autodata.tech/kia/ca/vehicle/#/select/primary/compare";
-		sendEmails(sendToEmail, gmailUsername, gmailPassword, sendEmailsList, Subject,
-				content + "\n\n\n URL: " + urlString);
-		System.out.println("\n\n*****************Complete!*******************\n");
 	}
 
 }
