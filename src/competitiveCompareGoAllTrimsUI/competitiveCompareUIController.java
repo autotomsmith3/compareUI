ca3acbc9bf511ebd057811d7b4226ccec795d49d
Zhoul <Zhoul@LZHOULT2.autodatacorp.org>
2021-12-16 15:00:26 -0500

    Minor updated

--- src/competitiveCompareGoAllTrimsUI/competitiveCompareUIController.java
+++ src/competitiveCompareGoAllTrimsUI/competitiveCompareUIController.java
@@ -933,7 +933,7 @@ public class competitiveCompareUIController extends Comlibs {
 						log.rwExcel("", "----" + brand + " Competitive Compare page Testing started-----" + (i + 1),
 								"");
 						// 1. ***********Competitive Compare**************
-//						CompetitiveCompareMonitor(driver, tBrowser, env, brand);
+						CompetitiveCompareMonitor(driver, tBrowser, env, brand);
 						// ***********Competitive Compare***************
 
 						// 2. ***********Reload failed URLs on Competitive Compare**************
@@ -946,13 +946,7 @@ public class competitiveCompareUIController extends Comlibs {
 						// ***********Competitive Compare***************
 
 						// 4. ***********read all en,fr,es URLs on Competitive Compare**************
-						ReLoadAllURLs(driver, env);
-//						
-//						System.out.println("Hello 23th  world!");
-//
-//						competitiveCompareUIController loadTextData=new competitiveCompareUIController(driver, env); 
-//						
-//						loadTextData.loadTextFromDataFolder(envDevice,"./data/"+envDevice+"AllURLs.txt");
+//						ReLoadAllURLs(driver, env);
 
 						// ***********read all en,fr,es URLs on Competitive Compare**************
 
